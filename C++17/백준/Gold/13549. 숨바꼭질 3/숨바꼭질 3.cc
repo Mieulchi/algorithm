e7cc0b51b3ec3c1bfd816431a1dac880755e7fb9
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2024-09-23 16:02:44 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int getTime(int n, int k) {
+	
+	int time = 0;
+	int tmp = k;
+	while (true) {
+		if (n > tmp / 2) {
+			if (n - tmp / 2 > tmp - n) {
+				return tmp - n;
+			}
+			else {
+				return n - tmp / 2;
+			}
+		}
+		if (tmp % 2 == 0) {
+			tmp /= 2;
+		}
+		else {
+			time += tmp % 2;
+			int a = getTime(n, (tmp + 1));
+			int b = getTime(n, (tmp - 1));
+			a > b ? time += b : time += a;
+			break;
+		}
+	}
+
+	return time;
+}
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+
+	int time = 0;
+	int zeroflag = 0;
+
+	if (n == 0) {
+		zeroflag = 1;
+	}
+
+	if (n >= k) {
+		time = n - k;
+		cout << time;
+	}
+	else {
+		if (n == 0) {
+			n = 1;
+		}
+
+		int gettime = getTime(n, k);
+		
+		if (zeroflag == 1) {
+			gettime++;
+		}
+		cout << gettime;
+	}
+}
