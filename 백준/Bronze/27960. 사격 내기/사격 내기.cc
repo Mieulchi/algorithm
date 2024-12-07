245348aa6ac18150a78f62a1298859945d4d6430
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2024-12-07 16:59:52 +0900

    [Bronze III] Title: 사격 내기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27960. 사격 내기/사격 내기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a, b;
+
+string arr[50];
+
+void solve() {
+	cout << (a ^ b);
+}
+
+int main() {
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	cin >> a >> b;
+
+	solve();
+}
