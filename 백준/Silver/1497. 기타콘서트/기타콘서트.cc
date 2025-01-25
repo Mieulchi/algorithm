81b54f3b393973f471b8f1a5d98304b6e97b016e
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-01-25 20:57:00 +0900

    [Silver I] Title: 기타콘서트, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1497. 기타콘서트/기타콘서트.cc
+++ 백준/Silver/1497. 기타콘서트/기타콘서트.cc
@@ -4,88 +4,53 @@ using namespace std;
 
 int n, m;
 pair<string, int[50]> s[10];
-int check[50];
 int scount;
 int ans;
 
-void checkBinary(int n) {
-	string st;
-	while (n) {
-		if (n % 2) {
-			st += '1';
-		}
-		else {
-			st += '0';
-		}
-		n /= 2;
-	}
-	int count = 0;
-	for (int i = st.length() - 1; i >= 0; i--) {
-		if (st[i] - '0') {
-			count++;
-			for (int j = 0; j < m; j++) {
-				check[j] |= s[i].second[j];
-			}
-		}
-	}
-
-	int flag = 0;
-	int tmp = 0;
-	for (int i = 0; i < m; i++) {
-		if (check[i]) {
-			flag = 1;
-			tmp++;
-		}
-	}
-	if (flag && (!scount || tmp >= scount)) {
-		if (tmp == scount) {
-			if (!ans || ans > count) {
-				ans = count;
-				scount = tmp;
-			}
-		}
-		else {
-			ans = count;
-			scount = tmp;
-		}
-	}
-}
-void init() {
-	for (int j = 0; j < 50; j++) {
-		check[j] = 0;
-	}
-}
-
 void solve() {
-	int k = pow(2, n);	
-	for (int i = 1; i < k; i++) {
-		checkBinary(i);
-		init();
-	}
-	if (!ans) {
-		ans = -1;
-	}
-}
+    int k = 1 << n; // 2^n
+    for (int mask = 1; mask < k; mask++) {
+        int check[50] = {0}; // 초기화
+        int count = 0;
 
+        for (int i = 0; i < n; i++) {
+            if (mask & (1 << i)) { // i번째 항목이 선택되었는지 확인
+                count++;
+                for (int j = 0; j < m; j++) {
+                    check[j] |= s[i].second[j];
+                }
+            }
+        }
+
+        int tmp = 0;
+        for (int i = 0; i < m; i++) {
+            if (check[i]) tmp++;
+        }
+
+        if (tmp > 0 && (scount < tmp || (scount == tmp && (ans == 0 || ans > count)))) {
+            scount = tmp;
+            ans = count;
+        }
+    }
+
+    if (!ans) {
+        ans = -1;
+    }
+}
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		string tmp;
-		cin >> s[i].first >> tmp;
-		for (int j = 0; j < tmp.length(); j++) {
-			if (tmp[j] == 'Y') {
-				s[i].second[j] = 1;
-			}
-			else {
-				s[i].second[j] = 0;
-			}
-		}
-	}
-	solve();
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        string tmp;
+        cin >> s[i].first >> tmp;
+        for (int j = 0; j < tmp.length(); j++) {
+            s[i].second[j] = (tmp[j] == 'Y') ? 1 : 0;
+        }
+    }
+    solve();
 
-	cout << ans;
+    cout << ans;
 }
