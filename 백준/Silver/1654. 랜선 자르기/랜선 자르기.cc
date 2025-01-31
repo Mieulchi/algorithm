075ac16c1032e6ba2717e548246664feadd16903
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-01-31 21:08:52 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 1188 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,13 +1,13 @@
-#include <iostream>
-#include <algorithm>
+#include <stdio.h>
 using namespace std;
 
 int k, n;
 long long arr[10001];
 int ans;
+int mymax;
 
 void solve() {
-	long long left = 1, right = arr[k - 1];
+	long long left = 1, right = mymax;
 	while (true) {
 		long long mid = (left + right) / 2;
 		int cnt = 0;
@@ -30,15 +30,14 @@ void solve() {
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	cin >> k >> n;
+	scanf("%d %d", &k, &n);
 	for (int i = 0; i < k; i++) {
-		cin >> arr[i];
+		scanf("%lld", &arr[i]);
+		if (arr[i] > mymax) {
+			mymax = arr[i];
+		}
 	}
-	sort(arr, arr + k);
 	solve();
 
-	cout << ans;
+	printf("%d", ans);
 }
