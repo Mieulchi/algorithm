660995f4db6ef6f79f572f7bedcfc37e1b395c4a
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-02-08 22:11:47 +0900

    [Silver II] Title: 세 수 고르기, Time: 144 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1503. 세 수 고르기/세 수 고르기.cc
+++ 백준/Silver/1503. 세 수 고르기/세 수 고르기.cc
@@ -4,26 +4,9 @@ using namespace std;
 
 int n, m;
 int arr[1001];
-int ans = 1001000001;
+int ans = (int)1e9;
 
 void solve() {
-	for (int i = 1; i <= 1000; i++) {
-		if (arr[i]) {
-			continue;
-		}
-		for (int j = i; j <= 1000; j++) {
-			if (arr[j]) {
-				continue;
-			}
-			for (int k = j; k <= 1001; k++) {
-				if (arr[k]) {
-					continue;
-				}
-				ans = min(ans, abs(n - i * j * k));
-			}
-		}
-	}
-
 	for (int i = 1; i <= 1000; i++) {
 		if (arr[i] == 1)continue;
 		for (int j = i; j <= 1000; j++) {
