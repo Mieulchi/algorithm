268a4ed94eae7ef574aabfccc0209a2371fd94a6
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-02-10 14:20:12 +0900

    [Gold III] Title: 눌러서 잠금 해제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2723. 눌러서 잠금 해제/눌러서 잠금 해제.cc
+++ 백준/Gold/2723. 눌러서 잠금 해제/눌러서 잠금 해제.cc
@@ -2,44 +2,41 @@
 using namespace std;
 
 int n, b;
-long long f[12];
+long long dp[12];
 long long ans[12];
 
-int comb(int a, int b) {
+int nCr(int n, int r) {
 	int child = 1, parent = 1;
-	for (int i = a; i > a - b; i--) {
+	for (int i = n; i > n - r; i--) {
 		child *= i;
 	}
-	for (int i = b; i >= 1; i--) {
+	for (int i = r; i > 0; i--) {
 		parent *= i;
 	}
-
 	return child / parent;
 }
 
 void solve() {
-	f[0] = 1;
+	dp[0] = 1;
 	for (int i = 1; i <= 11; i++) {
 		for (int j = 0; j < i; j++) {
-			f[i] += comb(i, j) * f[j];
+			dp[i] += nCr(i, j) * dp[j];
 		}
 	}
-
 	for (int i = 1; i <= 11; i++) {
 		for (int j = 1; j <= i; j++) {
-			ans[i] += comb(i, j) * f[j];
+			ans[i] += nCr(i, j) * dp[j];
 		}
 	}
 }
 
 int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
+	cin.tie(NULL);
 
 	cin >> n;
 
 	solve();
-
 	for (int i = 0; i < n; i++) {
 		cin >> b;
 		cout << ans[b] << '\n';
