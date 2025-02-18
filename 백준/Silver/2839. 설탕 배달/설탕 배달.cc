aefdfbd3e9c46c59ea4a0e8bea0ff4504ebc9575
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-02-18 10:44:11 +0900

    [Silver IV] Title: 설탕 배달, Time: 4 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int dp[5001];
+
+void recursive(int i) {
+	if (i + 3 <= 5000 && (!dp[i + 3] || dp[i + 3] > dp[i] + 1)) {
+		dp[i + 3] = dp[i] + 1;
+		recursive(i + 3);
+	}
+	if (i + 5 <= 5000 && (!dp[i + 5] || dp[i + 5] > dp[i] + 1)) {
+		dp[i + 5] = dp[i] + 1;
+		recursive(i + 5);
+	}
+}
+
+void solve() {
+	dp[3] = 1;
+	dp[5] = 1;
+	recursive(3);
+	recursive(5);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+
+	solve();
+	if (!dp[n]) {
+		dp[n] = -1;
+	}
+	cout << dp[n];
+}
