9ad28592b34cda6466115a9f1927c256de58eba0
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-04-02 10:36:24 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.cc
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+    int x;
+    scanf("%d", &x);
+    n-=x;
+    scanf("%d", &x);
+    n-=x;
+    scanf("%d", &x);
+    n-=x;
+    scanf("%d", &x);
+    n-=x;
+    scanf("%d", &x);
+    n-=x;
+    scanf("%d", &x);
+    n-=x;
+    scanf("%d", &x);
+    n-=x;
+    scanf("%d", &x);
+    n-=x;
+    scanf("%d", &x);
+    n-=x;
+    
+    printf("%d", n);
+}
