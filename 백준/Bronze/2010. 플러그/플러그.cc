5b4f5ed682b7adbd27cea5536a6cbc95a6a15510
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-05-07 10:26:49 +0900

    [Bronze III] Title: 플러그, Time: 36 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2010. 플러그/플러그.cc
+++ 백준/Bronze/2010. 플러그/플러그.cc
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    int t;
+	int n;
+	scanf("%d", &n);
+	int t;
     int ans = 0;
-    while(n--) {
-        scanf("%d", &t);
-        ans += (t - 1);
-    }
+	while (n--) {
+		scanf("%d", &t);
+		ans += t - 1;
+	}
     ans++;
     printf("%d", ans);
 }
