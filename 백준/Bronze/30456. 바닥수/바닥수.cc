98bae9fbe5d8b838752c723ff13d485929eeb59d
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-06-15 01:04:55 +0900

    [Bronze I] Title: 바닥수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30456. 바닥수/바닥수.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int n, l;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> l;
+	while (--l) {
+		cout << 1;
+	}
+	cout << n;
+}
