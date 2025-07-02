965d8fe545b8f55466be15d2075e855755b1a63d
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-07-02 17:29:20 +0900

    [Gold I] Title: 개구리 징검다리 건너기, Time: 120 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/21873. 개구리 징검다리 건너기/개구리 징검다리 건너기.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int now = 2;
+
+void toggle() {
+	now = now == 2 ? 1 : 2;
+}
+
+void solve() {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= i; j++) {
+			cout << now << ' ' << j << '\n';
+		}
+		toggle();
+	}
+	for (int i = 1; i <= N; i++) {
+		cout << now << ' ' << i << '\n';
+	}
+	toggle();
+	for (int i = N; i >= 1; i--) {
+		for (int j = N - i + 1; j <= N; j++) {
+			cout << now << ' ' << j << '\n';
+		}
+		toggle();
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N;
+	cout << N * N + 2 * N << '\n';
+	solve();
+}
