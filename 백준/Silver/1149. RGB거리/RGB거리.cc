74f02cee8763b12601d2f6b2556202cfb79f84dd
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-07-22 15:21:16 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,26 +1,36 @@
+#include <algorithm>
+using namespace std;
+
 #include <stdio.h>
 
-int arr[3][1000];
-int dp[3][1000];
+
+int dp[3];
 
 int main() {
     int n;
     scanf("%d", &n);
     
-    for(int i=0;i<n;i++) {
-        scanf("%d %d %d", &arr[0][i], &arr[1][i], &arr[2][i]);
-        if (i) {
-            dp[0][i] = arr[0][i] + (dp[2][i - 1] > dp[1][i - 1] ? dp[1][i - 1] : dp[2][i - 1]);
-            dp[1][i] = arr[1][i] + (dp[2][i - 1] > dp[0][i - 1] ? dp[0][i - 1] : dp[2][i - 1]);
-            dp[2][i] = arr[2][i] + (dp[1][i - 1] > dp[0][i - 1] ? dp[0][i - 1] : dp[1][i - 1]);
+    int r, g, b;
+    int N=n--;
+    while(N--) {
+        scanf("%d %d %d", &r, &g, &b);
+        int tmp[3];
+        if (N!=n) {
+            tmp[0] = r + min(dp[1], dp[2]);
+            tmp[1] = g + min(dp[0], dp[2]);
+            tmp[2] = b + min(dp[0], dp[1]);
+            
+            for(int i=0;i<3;i++){
+                dp[i] = tmp[i];
+            }
         }
         else {
-            dp[0][0] = arr[0][0];
-            dp[1][0] = arr[1][0];
-            dp[2][0] = arr[2][0];
+            dp[0] = r;
+            dp[1] = g;
+            dp[2] = b;
         }
     }
     
-    printf("%d", dp[1][n - 1] > dp[0][n - 1] ? (dp[2][n - 1] > dp[0][n - 1] ? dp[0][n - 1] : dp[2][n - 1] ): (dp[2][n - 1] > dp[1][n - 1] ? dp[1][n - 1] : dp[2][n - 1]));
+    printf("%d", min({dp[0], dp[1], dp[2]}));
     
 }
