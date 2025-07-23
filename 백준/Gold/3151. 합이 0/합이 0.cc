0a22d7dc8166f0275d9f3691c2154bc1d3a761b5
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-07-23 12:54:42 +0900

    [Gold IV] Title: 합이 0, Time: 20 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.cc
+++ 백준/Gold/3151. 합이 0/합이 0.cc
@@ -5,17 +5,37 @@ using namespace std;
 
 int N;
 int arr[10000];
+int n[20001];
+int p[20001];
+long long zero;
 long long ans;
 
 void solve() {
+	if (zero > 2) {
+		ans += zero * (zero - 1) * (zero - 2) / 6;
+	}
+	if (zero) {
+		for (int i = 1; i <= 10000; i++) {
+			if (n[i] && p[i]) {
+				ans += zero * (n[i] * p[i]);
+			}
+		}
+	}
+	
 	sort(arr, arr + N);
 
-	for (int i = 0; i < N - 1; i++) {
+	int left = lower_bound(arr, arr + N, 0) - arr;
+	int right = upper_bound(arr, arr + N, 0) - arr;
+
+	for (int i = right; i < N - 1; i++) {
 		for (int j = i + 1; j < N; j++) {
-			int sum = arr[i] + arr[j];
-			int left = lower_bound(arr + j + 1, arr + N, sum * - 1) - arr;
-			int right = upper_bound(arr + j + 1, arr + N, sum * - 1) - arr;
-			ans += (right - left);
+			ans += n[arr[i] + arr[j]];
+		}
+	}
+
+	for (int i = 0; i < left - 1; i++) {
+		for (int j = i + 1; j < left; j++) {
+			ans += p[(arr[i] + arr[j]) * -1];
 		}
 	}
 }
@@ -27,6 +47,10 @@ int main() {
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		cin >> arr[i];
+		if (!arr[i]) {
+			zero++;
+		}
+		arr[i] > 0 ? p[arr[i]]++ : n[arr[i] * -1]++;
 	}
 	solve();
 	cout << ans;
