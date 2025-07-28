9dc2eec2a602e9069b5b441107e304e179d2bc94
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-07-29 00:41:32 +0900

    [Silver IV] Title: 수 찾기, Time: 72 ms, Memory: 1504 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,34 +1,8 @@
-#include <iostream>
 #include <algorithm>
 using namespace std;
 
-int n, m;
-int arr[100001];
-int ans;
-
-void solve() {
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-
-	sort(arr, arr + n);
-	cin >> m;
-	for (int i = 0; i < m; i++) {
-		int t;
-		cin >> t;
-		if (binary_search(arr, arr + n, t)) {
-			cout << 1 << '\n';
-		}
-		else {
-			cout << 0 << '\n';
-		}
-	}
-}
-
-int main() {	
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
-	solve();
+#include <stdio.h>
+int arr[100000];
+int main() {
+    int n,m,t;scanf("%d",&n);for(int i=0;i<n;i++){scanf("%d",&arr[i]);}sort(arr, arr + n);scanf("%d",&m);while(m--){scanf("%d",&t);printf("%d\n",binary_search(arr,arr+n,t)?1:0);}
 }
