b042e4e1849f8599d6954d246df4d2cdab45da62
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-08-03 02:09:43 +0900

    [Silver II] Title: 조합 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.cc
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.cc
@@ -2,31 +2,35 @@
 #include <algorithm>
 using namespace std;
 
-long long n, m;
-long long ans;
-long long a, b;
+int n, m;
+int ans;
+
+long long cnt(int target, long long div) {
+    int o = div;
+    long long ret = 0;
+    while (div <= target) {
+        ret += target / div;
+        div *= o;
+    }
+    return ret;
+}
 
 void solve() {
-	long long i = 5;
-	while (i <= n) {
-		a = a + (n / i) - (n - m) / i - m / i;
-		i *= 5;
-	}
-	i = 2;
-	while (i <= n) {
-		b = b + (n / i) - (n - m) / i - m / i;
-		i *= 2;
-	}
-	ans = min(a, b);
+    long long a, b, c, d, e, f;
+    a = cnt(n, 5);
+    b = cnt(n, 2);
+    c = cnt(n - m, 5);
+    d = cnt(n - m, 2);
+    e = cnt(m, 5);
+    f = cnt(m, 2);
+    ans = min(a - c - e, b - d - f);
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	cin >> n >> m;
-
-	solve();
-
-	cout << ans;
-}
+    cin >> n >> m;
+    solve();
+    cout << ans;
+}   
