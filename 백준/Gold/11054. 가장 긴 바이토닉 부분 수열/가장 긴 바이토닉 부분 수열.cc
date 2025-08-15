809db12302a0fcf0d1fd2180373f1f016da46801
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2025-08-15 09:59:38 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -1,58 +1,46 @@
 #include <iostream>
-#include <climits>
+#include <algorithm>
 using namespace std;
 
-int sq[1001];
-int dp1[1001];
-int dp2[1001];
+int n;
+int arr[1000];
+int dp[2][1000];
+int ans;
+
+void solve() {
+	for (int i = 0; i < n; i++) {
+		int mx = 0;
+		for (int j = 0; j < i; j++) {
+			if (dp[0][j] > mx && arr[j] < arr[i]) {
+				mx = dp[0][j];
+			}
+		}
+		dp[0][i] = mx + 1;
+	}
+	for (int i = n - 1; i >= 0; i--) {
+		int mx = 0;
+		for (int j = n - 1; j > i; j--) {
+			if (dp[1][j] > mx && arr[j] < arr[i]) {
+				mx = dp[1][j];
+			}
+		}
+		dp[1][i] = mx + 1;
+	}
+	for (int i = 0; i < n; i++) {
+		ans = max(ans, dp[0][i] + dp[1][i] - 1);
+	}
+}
 
 int main() {
-	int n;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
 	cin >> n;
-
-	int length = 0;
-
 	for (int i = 0; i < n; i++) {
-		cin >> sq[i];
+		cin >> arr[i];
 	}
 
-	for (int i = 0; i < n; i++) {
-		dp1[i] = 1;
-		int max = 0;
-		for (int j = 0; j < i; j++) {
-			if (sq[j] < sq[i] && dp1[j] > max) {
-				max = dp1[j];
-			}
-		}
-		dp1[i] += max;
-	}
-
-	for (int i = n - 1; i >= 0; i--) {
-		dp2[i] = 1;
-		int min = 0;
-		for (int j = n - 1; j >= i; j--) {
-			if (sq[j] < sq[i] && dp2[j] > min) {
-				min = dp2[j];
-			}
-		}
-		dp2[i] += min;
-	}
-
-	int result = 0;
-	int max = 0;
-	for (int i = 0; i < n; i++) {
-		int op1 = dp1[i];
-		int maxtmp = 0;
-		for (int j = i + 1; j < n; j++) {
-			if (dp2[j] > maxtmp && sq[j] != sq[i]) {
-				maxtmp = dp2[j];
-			}
-		}
-		if (op1 + maxtmp > result) {
-			result = op1 + maxtmp;
-		}
-	}
-
-
-	cout << result;
+	solve();
+	
+	cout << ans;
 }
