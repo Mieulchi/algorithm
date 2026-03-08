370c9a31370ded11295faedc2a1004a06b8395e1
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2026-03-08 10:22:19 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 88 ms, Memory: 10216 KB -BaekjoonHub

--- 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.cc
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.cc
@@ -6,9 +6,8 @@ using namespace std;
 
 int n, ans;
 int arr[20][20];
-int dp[21][1 << 20];
+int dp[1 << 20];
 int cnt[1 << 20];
-//dp[i][mask] = i번 사람까지 이미 일을 햇을 때에 mask 상태를 만족하는 상태의 최소 비용
 
 void solve() {
 	for (int mask = 0; mask < (1 << n); ++mask) {
@@ -21,21 +20,21 @@ void solve() {
 		cnt[mask] = count;
 	}
 
-	dp[0][0] = 0;
-	for (int i = 1; i <= n; ++i) {
-		for (int mask = 0; mask < (1 << n); ++mask) {
-			if (cnt[mask] == i) {
-				for (int j = 0; j < n; ++j) {
-					int prev = mask - (1 << j);
-					if (prev >= 0 && dp[i - 1][prev] != INF) {
-						dp[i][mask] = min(dp[i][mask], dp[i - 1][prev] + arr[i - 1][j]);
-					}
-				}
+	dp[0] = 0;
+	for (int mask = 0; mask < (1 << n); ++mask) {
+		int num = cnt[mask] - 1;
+
+		for (int i = 0; i < n; ++i) {
+			if (mask & (1 << i)) {
+				int prev = mask & ~(1 << i);
+
+				dp[mask] = min(dp[mask], dp[prev] + arr[num][i]);
 			}
 		}
 		
+
 	}
-	ans = dp[n][(1 << n) - 1];
+	ans = dp[(1 << n) - 1];
 
 }
 
@@ -50,11 +49,10 @@ int main() {
 			cin >> arr[i][j];
 		}
 	}
-	for (int i = 0; i <= n; ++i) {
-		for (int mask = 0; mask < (1 << n); ++mask) {
-			dp[i][mask] = INF;
-		}
+	for (int mask = 0; mask < (1 << n); ++mask) {
+		dp[mask] = INF;
 	}
+
 
 	solve();
 
