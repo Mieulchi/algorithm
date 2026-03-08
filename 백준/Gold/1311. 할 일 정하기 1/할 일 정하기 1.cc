51c1ba9d916f8457aba229a1ea0020cc14cbac45
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2026-03-08 10:23:07 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 64 ms, Memory: 6120 KB -BaekjoonHub

--- 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.cc
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.cc
@@ -7,22 +7,12 @@ using namespace std;
 int n, ans;
 int arr[20][20];
 int dp[1 << 20];
-int cnt[1 << 20];
 
 void solve() {
-	for (int mask = 0; mask < (1 << n); ++mask) {
-		int count = 0;
-		for (int i = 0; i < n; ++i) {
-			if ((mask >> i) & 1) {
-				count++;
-			}
-		}
-		cnt[mask] = count;
-	}
 
 	dp[0] = 0;
 	for (int mask = 0; mask < (1 << n); ++mask) {
-		int num = cnt[mask] - 1;
+		int num = __builtin_popcount(mask) - 1;
 
 		for (int i = 0; i < n; ++i) {
 			if (mask & (1 << i)) {
