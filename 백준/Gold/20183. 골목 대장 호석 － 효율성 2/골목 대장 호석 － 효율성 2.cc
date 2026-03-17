40bfa4ece18b30bd34c9b87da7eb3ba59559125f
Mieulchi <147303547+Mieulchi@users.noreply.github.com>
2026-03-17 22:15:39 +0900

    [Gold I] Title: 골목 대장 호석 - 효율성 2, Time: 284 ms, Memory: 27560 KB, Score: 28 point -BaekjoonHub

---
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+#define INF 1000000007
+typedef pair<int, pair<int, int>> ppp;
+typedef long long ll;
+
+int n, m, a, b, ans = INF;
+ll c;
+
+vector<ppp> v[100001];
+int dist[100001];
+int minDist[100001];
+
+struct cmp {
+	bool operator()(ppp p1, ppp p2) {
+		return p1.second.first > p2.second.first;
+	}
+};
+
+void solve() {
+	for (int i = 1; i <= n; ++i) {
+		minDist[i] = dist[i] = INF;
+	}
+
+	priority_queue<ppp, vector<ppp>, cmp> pq;
+
+	pq.push({ a, { 0, 0 } });
+
+	while (!pq.empty()) {
+		ppp front = pq.top();
+		pq.pop();
+		int now = front.first;
+		int w = front.second.first;
+		int shame = front.second.second;
+
+		if (now == b && w && w <= c) {
+			ans = min(ans, shame);
+		}
+
+		if (dist[now] < w || w > c) {
+			continue;
+		}
+
+
+		dist[now] = w;
+		
+		for (ppp p : v[now]) {
+			int next = p.first;
+			int nW = p.second.first;
+			int nShame = p.second.second;
+			int distance = dist[now] + nW;
+			if (dist[next] > distance && distance < minDist[next]) {
+				minDist[next] = distance;
+				pq.push({ next, {distance, max(shame, nShame)} });
+			}
+		}
+	}
+	if (ans == INF) {
+		ans = -1;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin >> n >> m >> a >> b >> c;
+	int f, t, w;
+	while (m--) {
+		cin >> f >> t >> w;
+		v[f].push_back({ t, {w, w} });
+		v[t].push_back({ f, {w, w} });
+	}
+	solve();
+
+	cout << ans;
+}
